16c6895f53f341fc3082cc5540e99af614292915
ivan <ivan_0805@mail.ru>
2021-09-27 14:18:52 +0300

    Minor fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,20 @@ const popup = document.querySelector('.popup')
 const popupOpenBtn = document.querySelector('.profile__edit-icon')
 const popupCloseBtn = popup.querySelector('.popup__close')
 const popupSaveBtn = popup.querySelector('.popup__submit')
+const body = document.querySelector('.body')
+
+function closePopup(){
+    popup.classList.remove('popup_opened')
+    body.classList.remove('body_no-scroll')
+}
+
+function openPopup(){
+    popup.classList.add('popup_opened')
+    body.classList.add('body_no-scroll')
+}
 
 popupOpenBtn.addEventListener('click', function () {
-    popup.classList.add('popup_opened')
+    openPopup()
 
     const fio = document.querySelector('.profile__name').textContent
     const occupation = document.querySelector('.profile__occupation').textContent
@@ -14,5 +25,22 @@ popupOpenBtn.addEventListener('click', function () {
 })
 
 popupCloseBtn.addEventListener('click', function () {
-    popup.classList.remove('popup_opened')
+    closePopup()
+})
+
+popupSaveBtn.addEventListener('click', function (event) {
+    event.preventDefault()
+    closePopup()
+
+    const fio = popup.querySelector('.popup__input-name').value
+    const occupation = popup.querySelector('.popup__input-occupation').value
+
+    document.querySelector('.profile__name').textContent = fio
+    document.querySelector('.profile__occupation').textContent = occupation
+})
+
+popup.addEventListener('click', function (event) {
+    if (event.target === event.currentTarget) {
+        closePopup()
+    }
 })
