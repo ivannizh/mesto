5072526428e53cc10549f7f4efd8af7e45068144
ivan <ivan_0805@mail.ru>
2021-09-28 12:55:35 +0300

    Minor fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,27 @@ const popup = document.querySelector('.popup')
 const popupOpenBtn = document.querySelector('.profile__edit-icon')
 const popupCloseBtn = popup.querySelector('.popup__close')
 const popupForm = popup.querySelector('.popup__form')
-const body = document.querySelector('.body')
+
+const fio_on_page = document.querySelector('.profile__name')
+const occupation_on_page = document.querySelector('.profile__occupation')
+
+const fio_on_form = popup.querySelector('.popup__input-occupation')
+const occupation_on_form = popup.querySelector('.popup__input-name')
+
 
 function closePopup(){
     popup.classList.remove('popup_opened')
-    body.classList.remove('body_no-scroll')
 }
 
 function openPopup(){
     popup.classList.add('popup_opened')
-    body.classList.add('body_no-scroll')
 }
 
 popupOpenBtn.addEventListener('click', function () {
     openPopup()
 
-    const fio = document.querySelector('.profile__name').textContent
-    const occupation = document.querySelector('.profile__occupation').textContent
-
-    popup.querySelector('.popup__input-name').value = fio
-    popup.querySelector('.popup__input-occupation').value = occupation
+    occupation_on_form.value = fio_on_page.textContent
+    fio_on_form.value = occupation_on_page.textContent
 })
 
 popupCloseBtn.addEventListener('click', function () {
@@ -32,15 +33,6 @@ popupForm.addEventListener('submit', function (event) {
     event.preventDefault()
     closePopup()
 
-    const fio = popup.querySelector('.popup__input-name').value
-    const occupation = popup.querySelector('.popup__input-occupation').value
-
-    document.querySelector('.profile__name').textContent = fio
-    document.querySelector('.profile__occupation').textContent = occupation
-})
-
-popup.addEventListener('click', function (event) {
-    if (event.target === event.currentTarget) {
-        closePopup()
-    }
+    fio_on_page.textContent = fio_on_form.value
+    occupation_on_page.textContent = occupation_on_form.value
 })
