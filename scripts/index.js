21ba414c25f1c63b4cf5d2f484c5d934a439bf74
ivan <ivan_0805@mail.ru>
2021-09-30 12:37:31 +0300

    Minor fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const popup = document.querySelector('.popup')
-const popupOpenBtn = document.querySelector('.profile__edit-icon')
+const popupOpenBtn = document.querySelector('.profile__edit-button')
 const popupCloseBtn = popup.querySelector('.popup__close')
 const popupForm = popup.querySelector('.popup__form')
 
 const fioOnPage = document.querySelector('.profile__name')
 const occupationOnPage = document.querySelector('.profile__occupation')
 
-const fioOnForm = popup.querySelector('.popup__input_name')
-const occupationOnForm = popup.querySelector('.popup__input_occupation')
+const fioOnForm = popup.querySelector('.popup__input_type_name')
+const occupationOnForm = popup.querySelector('.popup__input_type_occupation')
 
 
 function closePopup() {
@@ -21,15 +21,15 @@ function openPopup() {
     popup.classList.add('popup_opened')
 }
 
-popupOpenBtn.addEventListener('click', openPopup)
-
-popupCloseBtn.addEventListener('click', closePopup)
-
-popupForm.addEventListener('submit', function(event) {
+function formSubmitHandler(event) {
     event.preventDefault()
 
     fioOnPage.textContent = fioOnForm.value
     occupationOnPage.textContent = occupationOnForm.value
 
     closePopup()
-})
+}
+
+popupOpenBtn.addEventListener('click', openPopup)
+popupCloseBtn.addEventListener('click', closePopup)
+popupForm.addEventListener('submit', formSubmitHandler)
